b3ee2df9a9de450b36ef4c7f773295e7b17e783e
juanrovillegas26 <55925550+juanrovillegas26@users.noreply.github.com>
2021-11-22 13:23:33 -0500

    server

---
+++ src/main/java/com/usa/ProyectoReto1Application.java
@@ -0,0 +1,13 @@
+package com.usa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoReto1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoReto1Application.class, args);
+    }
+
+}
